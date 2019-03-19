50e82928c78f870d1bc66b1d08c06ef271688388
Mason Hawver <33504840+MasonJohnHawver42@users.noreply.github.com>
2019-03-18 22:04:13 -0500

    Update World.py

--- World.py
+++ World.py
@@ -125,17 +125,5 @@ class World:
     def addMousePredator(self):
         m_pred = MousePredator(self)
         self.predators.append(m_pred)
-
-
-def main():
-    size = (1000, 1000)
-    world = World(size[0], size[1])
-    world.addRndBoids(40)
-    world.BoidTrackbars()
-    world.WorldTrackbars()
-
-    world.simulate()
-
-
-if __name__ == '__main__':
-    main()
+        
+     
